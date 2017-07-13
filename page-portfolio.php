c76a49b66bc56fbdc411102de123a650abe929c0
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-07-13 17:46:08 -0400

    Anadiendo templating para portfolio

---
+++ page-portfolio.php
@@ -0,0 +1,29 @@
+<?php 
+
+get_header();
+
+if(have_posts()):
+    while (have_posts()) : the_post(); ?>
+    <article class="post page">
+
+        <div class="row">
+            <div class="col-xs-2">
+                <h2>
+                    <?php the_title() ?>
+                </h2>
+            </div>
+            <div class="col-xs-10">        
+                <?php the_content() ?>
+            </div>
+        </div>
+    </article>
+        
+    <?php endwhile; 
+    else : 
+        echo '<p>No content found</p>';
+endif;
+
+get_footer();
+
+?>
+
