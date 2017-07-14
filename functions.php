3511d344f0a414ad478510b84c7ae1db48984603
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-07-14 16:54:50 -0400

    Child pages menu

--- functions.php
+++ functions.php
@@ -27,3 +27,17 @@ function special_nav_class ($classes, $item) {
     }
     return $classes;
 }
+
+// get top ancestor
+
+function  get_top_ancestor_id(){
+
+    global $post;
+
+    if($post->post_parent){
+        $ancestors = array_reverse(get_post_ancestors( $post->ID ));
+        return $ancestors[0];
+    }
+
+    return $post->ID;
+}
